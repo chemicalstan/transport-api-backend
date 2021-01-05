5b8f5c1a22114668de64b70306918377f354b193
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-05 22:03:04 +0100

    database query structure designed

---
+++ app/db/dev/dbQuery.js
@@ -0,0 +1,23 @@
+const pool = require('./pool');
+
+
+module.exports= {
+    /**
+   * DB Query
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} object
+   */
+
+   query(queryText, params){
+       return new Promise((resolve, reject)=>{
+            pool.query(queryText, params)
+                .then(res=>{
+                    resolve(res);
+                })
+                .catch(err=>{
+                    reject(err);
+                })
+       });
+   }
+}
---
+++ app/db/dev/pool.js
@@ -0,0 +1,10 @@
+// const Pool = require('pg');
+// require('dotenv').config();
+import {Pool} from 'pg'
+import {dotenv} from 'dotenv';
+dotenv.config()
+
+const databaseConfig = {connectionString: process.env.DATABASE_URL};
+const pool = new Pool(databaseConfig);
+
+module.exports = pool;
