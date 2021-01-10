247bd9e4b5bc8817c558957c8854a17d28982d56
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-10 19:51:32 +0100

    Integrate status.js containing various response messages and status code

---
+++ app/helpers/status.js
@@ -0,0 +1,25 @@
+const successMessage = { status: "success" };
+const errorMessage = { status: "error" };
+
+const status = {
+  success: 200,
+  error: 500,
+  notfound: 404,
+  unauthorized: 401,
+  conflict: 409,
+  created: 201,
+  bad: 400,
+  nocontent: 204
+};
+
+const trip_status = {
+    active: 1.00,
+    cancelled: 2.00
+}
+
+module.exports = {
+    successMessage,
+    errorMessage,
+    status,
+    trip_status
+}
