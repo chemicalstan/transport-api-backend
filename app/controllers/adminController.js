e3502b0913de2207f12ec7775fffcac70dcee94e
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-12 00:10:54 +0100

    Integrate update user to admin

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -75,7 +75,7 @@ const createAdmin = async (req, res) => {
       const { rows } = dbQuery.query(createUserQuery, values)
       const dbResponse = rows[0];
       delete dbResponse.password
-      const token = generateUserToken(dbResponse.email, dbResponse.id, is_admin, dbResponse.first_name, dbResponse.last_name);
+      const token = generateUserToken(dbResponse.email, dbResponse.id, dbResponse.is_admin, dbResponse.first_name, dbResponse.last_name);
       successMessage.data = dbResponse;
       successMessage.data.token = token;
       return res.status(status.created).send(successMessage);
@@ -86,6 +86,57 @@ const createAdmin = async (req, res) => {
         return res.status(status.conflict).send(errorMessage);
       }
     errorMessage.error = 'Operation was not successful'
-    return res.status(status.conflict).send(errorMessage)l
+    return res.status(status.conflict).send(errorMessage)
   }
 };
+
+/**
+ * Update User to an Admin 
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} Updated User
+ */
+
+ const updateUserToAdmin = (req, res)=>{
+  const { id } = req.params;
+  const { isAdmin } = req.body;
+
+  const { is_admin } = req.user;
+  // Check if user is an admin
+  if (!is_admin === true) {
+    errorMessage.error = 'Sorry You are unauthorized to make a user an admin';
+    return res.status(status.bad).send(errorMessage);
+  }
+  // Validate isAdmin status from payload
+  if(isAdmin === ''){
+    errorMessage.error = 'Admin status is needed';
+    return res.status(status.ba).send(errorMessage);
+  }
+  const findUserQuery = 'SELECT * FROM users WHERE id=$1';
+  const updateUserQuery = `UPDATE users SET is_admin=$1 WHERE id=$2 returning *`;
+  try {
+    const { rows } = await dbQuery(findUserQuery, id);
+    const dbResponse = rows[0]
+    if (!dbResponse) {
+      errorMessage.error = 'User Cannot be found';
+      return res.status(status.notfound).send(errorMessage);
+    }
+    const values = {
+      isAdmin,
+      id
+    };
+    const response = dbQuery(updateUserQuery, values);
+    const dbResult = response.rows[0];
+    delete dbResult.password;
+    successMessage.data = dbResult;
+    return res.status(status.success).send(successMessage);
+  } catch (error) {
+    errorMessage.error = 'Operation was not successful';
+    return res.status(status.error).send(errorMessage);
+  }
+ };
+
+ module.exports = {
+   createAdmin,
+   updateUserToAdmin
+ }
