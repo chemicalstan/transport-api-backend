fb83cfa1450f055a3a5babb7db73cd5ab6f66620
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-11 13:31:47 +0100

    Integrate hash password validation

---
+++ app/controllers/adminController.js
@@ -0,0 +1,3 @@
+const moment = require('moment');
+const validations = require('../helpers/validatons');
+const { isEmpty, isValidEmail, validatePassword, empty } = validations
--- app/helpers/validatons.js
+++ app/helpers/validatons.js
@@ -2,6 +2,7 @@
 
 const env = require('../../env');
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
 
 /**
    * isValidEmail helper method
@@ -69,9 +70,44 @@ const jwt = require('jsonwebtoken')
     return token;
   }
 
+/**
+ * Hash password
+ * @param {string} password
+ * @returns {string} hashed password
+ */
+
+const hashPassword = (password) => {
+    const saltRounds = 10;
+    bcrypt.getSalt(saltRounds, (err, salt)=>{
+        if(err) throw err;
+        bcrypt.hash(password, salt, (err, hash)=>{
+            if(err) throw err;
+            return hash
+        })
+    })
+  }
+
+  /**
+ * Generate Token
+ * @param {string} id
+ * @returns {string} token
+ */
+
+const generateUserToken = (email, id, is_admin, first_name, last_name) => {
+    const token = jwt.sign({
+        email,
+        user_id: id,
+        is_admin,
+        first_name,
+        last_name
+    }, env.secrete, {expiresIn: '3d'});
+    return token;
+  }
+
   module.exports = {
       isValidEmail,
       validatePassword,
       isEmpty,
-      empty
+      empty,
+      generateUserToken,
   }
