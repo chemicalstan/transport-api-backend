22c65ca811c817cf9433aca26eaa27575db2c841
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-13 09:30:41 +0100

    Integrate get all trips controller method

--- app/controllers/tripController.js
+++ app/controllers/tripController.js
@@ -5,12 +5,12 @@ const moment = require("moment"),
   { isEmpty, empty } = validation,
   { successMessage, errorMessage, status, trip_status } = Status;
 
-  /**
-   * Create Trip
-   * @param {object} req 
-   * @param {object} res 
-   * @returns {object} reflection object
-   */
+/**
+ * Create Trip
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} reflection object
+ */
 const createTrip = async (req, res) => {
   // Retrieve data from payload
   const { bus_id, origin, destination, trip_date, fare } = req.body;
@@ -29,25 +29,49 @@ const createTrip = async (req, res) => {
       empty(trip_date),
     empty(fare))
   ) {
-    errorMessage.error = "bus id, origin, destination, trip date and fare field cannot be empty";
+    errorMessage.error =
+      "bus id, origin, destination, trip date and fare field cannot be empty";
     return res.status(status.bad).send(errorMessage);
   }
   const createTripQuery = `INSERT INTO trip(bus_id, origin, destination, trip_date, fare, created_on) VALUES($1, $2, $3, $4, $5, $6) returning *`;
   const values = {
-      bus_id,
-      origin,
-      destination,
-      trip_date,
-      fare,
-      created_on
+    bus_id,
+    origin,
+    destination,
+    trip_date,
+    fare,
+    created_on
   };
   try {
-      const { rows } = await dbQuery(createTripQuery, values);
-      const dbResponse = rows[0];
-      successMessage.data = dbResponse
-      return res.status(status.created).send(successMessage)
+    const { rows } = await dbQuery(createTripQuery, values);
+    const dbResponse = rows[0];
+    successMessage.data = dbResponse;
+    return res.status(status.created).send(successMessage);
   } catch (error) {
-      errorMessage.error = 'There was a problem creating trip';
-      return res.status(status.error).send(errorMessage);
+    errorMessage.error = "There was a problem creating trip";
+    return res.status(status.error).send(errorMessage);
+  }
+};
+
+/**
+ * Get all trips
+ * @param {object} req 
+ * @param {object} res 
+ * @returns {object} trips array
+ */
+const getAllTrips = async (req, res) => {
+  const getAllTripsQuery = `SELECT * FROM trip ORDER BY id DESC`;
+  try {
+    const { rows } = await dbQuery(getAllTripsQuery);
+    const dbResponse = rows;
+    if (!dbResponse[0]) {
+      errorMessage.error = "There are no trips";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = dbResponse;
+    return res.send(status.success).send(successMessage);
+  } catch (error) {
+    errorMessage.error = 'Operation was not successful';
+    return res.status(status.error).send(errorMessage);
   }
 };
