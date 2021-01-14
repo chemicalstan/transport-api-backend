4cb3af69c5bb19f73061232de05b0beadbf5ea76
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-14 19:30:52 +0100

    Integrate get all booking controller method

--- app/controllers/bookingController.js
+++ app/controllers/bookingController.js
@@ -1,7 +1,7 @@
 const moment = require("moment");
 const { empty } = require("../helpers/validatons");
 const { successMessage, errorMessage, status } = require("../helpers/status");
-const dbQuery = require('../db/dev/dbQuery');
+const dbQuery = require("../db/dev/dbQuery");
 
 /**
  * Add A Booking
@@ -23,19 +23,65 @@ const createBooking = async (req, res) => {
   booking(user_id, trip_id, bus_id, trip_date, seat_number, first_name, last_name, email, created_on)
   VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
   returning *`;
-  const values = [user_id, trip_id, bus_id, trip_date, seat_number, first_name, last_name, email, created_on];
+  const values = [
+    user_id,
+    trip_id,
+    bus_id,
+    trip_date,
+    seat_number,
+    first_name,
+    last_name,
+    email,
+    created_on
+  ];
   try {
-    const {rows}  = await dbQuery(createBookingQuery, values);
+    const { rows } = await dbQuery(createBookingQuery, values);
     const dbResponse = rows[0];
     successMessage.data = dbResponse;
-    return res.status(status.success).send(successMessage)
+    return res.status(status.success).send(successMessage);
   } catch (error) {
     //   seat_number row is unique. checking if seat is already taken
-      if (error.routine === '_bt_check_unique') {
-        errorMessage.error = 'Seat Number is taken already';
-        return res.status(status.conflict).send(errorMessage);
+    if (error.routine === "_bt_check_unique") {
+      errorMessage.error = "Seat Number is taken already";
+      return res.status(status.conflict).send(errorMessage);
+    }
+    errorMessage.error = "Unable to create booking";
+    return res.status(status.error).send(errorMessage);
+  }
+};
+
+const getAllBookings = async (req, res) => {
+  const { user_id, is_admin } = req.user;
+
+  if (!is_admin === true) {
+    const getUserBookingsQuery = `SELECT * FROM booking WHERE user_id = $1`;
+    try {
+      const { rows } = await dbQuery(getUserBookingsQuery, [user_id]);
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = "You have no bookings";
+        return res.status(status.notfound).send(errorMessage);
       }
-    errorMessage.error = 'Unable to create booking';
+      successMessage.data = dbResponse;
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
+      errorMessage.error = "Could not user's bookings";
+      return res.status(status.error).send(errorMessage);
+    }
+  }
+
+  const getAllBookingsQuery = `SELECT * FROM booking ORDER BY id DESC`;
+  try {
+    const { rows } = await dbQuery(getAllBookingsQuery);
+    const dbResponse = rows;
+    if (dbResponse === undefined) {
+      errorMessage.error = "There are no bookings";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = dbResponse;
+    return res.status(status.success).send(successMessage);
+  } catch (error) {
+    errorMessage.error = "An error Occured";
     return res.status(status.error).send(errorMessage);
   }
 };
