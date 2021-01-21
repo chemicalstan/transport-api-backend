b77ba061fbfeaaa61f8d824658ff7f8aa824f7e3
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-21 01:21:19 +0100

    Use urlencoded middleware

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 // Add middleware for parsing URL encoded bodies (which are usually sent by browser)
 app.use(cors());
 // Add middleware for parsing JSON and urlencoded data and populating `req.body`
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); ///// use body parser
 app.use(express.json());
 
 app.use('/api/v1', userRoute);
